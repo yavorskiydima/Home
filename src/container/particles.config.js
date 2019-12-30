6025d4fff164d5bd8a4b58f4047ed658eb2cb5f4
Dima Yavorskiy <33015386+yavorskiydima@users.noreply.github.com>
2019-12-30 16:22:12 +0500

    feat(preview): added icon

--- src/container/particles.config.js
+++ src/container/particles.config.js
@@ -1,7 +1,7 @@
 export const config = {
     "particles": {
         "number": {
-            "value": 80,
+            "value": 100,
             "density": {
                 "enable": true,
                 "value_area": 710.2328774690454
--- src/container/preview/preview.js
+++ src/container/preview/preview.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Text, } from '../../shared/components';
 import photo from './photo.jpg'
 import { Img, } from './preview.styled';
+import { GithubIcon, } from '../../shared/icon/github'
+import { FacebookIcon, } from '../../shared/icon/facebook'
 
 export class Preview extends Component {
     render() {
@@ -9,9 +11,14 @@ export class Preview extends Component {
             <Text align="center">
                 <h1>Привет! Меня зовут Дима.</h1>
                 <p>В 2020 начну суда заливать какие-нибудь свои проекты, правда не знаю зачем... пусть будет</p>
-                <p>А сейчас я вставлю свое лицо, что бы разбавить текст картинкой, и потом влуплю ссылки на соцсети</p>
+                <p>А сейчас я вставлю свое лицо, что бы разбавить текст картинкой, и потом вставлю ссылки на соцсети</p>
                 <p>А еще это все довольно поганно выглядит на мобилке</p>
                 <Img src={photo} />
+                <p>
+                    <a href="https://github.com/yavorskiydima" target="_blank" rel="noopener noreferrer"><GithubIcon /></a>
+                    <span>  </span>
+                    <a href="https://www.facebook.com/yavorskiydima" target="_blank" rel="noopener noreferrer"><FacebookIcon /></a>
+                </p>
             </Text>
         )
     }
